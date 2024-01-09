64fc1d3b5d56b29a560f41e10f82018bbd8d3126
SMToad <bonina9179@gmail.com>
2024-01-09 22:07:19 +0300

    Removing the uneccessary state

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,26 @@ import GameBoard from "./components/GameBoard/GameBoard";
 import Log from "./components/Log/Log";
 import { useState } from "react";
 
+function getActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X')
+    currentPlayer = 'O';
+  return currentPlayer;
+}
+
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
+  //const [activePlayer, setActivePlayer] = useState("X");
   const [gameTurns, setGameTurns] = useState([]);
 
-  function handleSelectCell(rowIndex, colIndex){
-    setActivePlayer(currActivePlayer => currActivePlayer === 'X' ? 'O' : 'X');
-    setGameTurns(currGameTurns => {
-      let currentPlayer = 'X';
-      if(currGameTurns.length > 0 && currGameTurns[0].player === 'X')
-        currentPlayer = 'O';
+  const activePlayer = getActivePlayer(gameTurns);
 
+  function handleSelectCell(rowIndex, colIndex){
+    //setActivePlayer(currActivePlayer => currActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(currGameTurns => {
+      //let currentPlayer = 'X';
+      //if(currGameTurns.length > 0 && currGameTurns[0].player === 'X')
+       // currentPlayer = 'O';
+      let currentPlayer = getActivePlayer(currGameTurns);
       const updatedGameTurns = [
         {
           cell : {row: rowIndex, column: colIndex},
